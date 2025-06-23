ac899ae62521dacb1d9839febcbd4f0a119543c8
agapeace <damiragadilov1@gmail.com>
2025-06-23 15:46:47 +0500

    UPDATE: Prefetch funciton W\OUT DISPATCHGROUP

---
+++ Whatever/SecondScreen/ConvertPositionImage.swift
@@ -0,0 +1,8 @@
+//
+//  ConvertPositionImage.swift
+//  Whatever
+//
+//  Created by Damir Agadilov  on 18.06.2025.
+//
+
+import Foundation
---
+++ Whatever/SecondScreen/PlayerPositionCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  PlayerPositionCollectionViewCell.swift
+//  Whatever
+//
+//  Created by Damir Agadilov  on 18.06.2025.
+//
+
+import UIKit
+
+class PlayerPositionCollectionViewCell: UICollectionViewCell {
+    
+}
