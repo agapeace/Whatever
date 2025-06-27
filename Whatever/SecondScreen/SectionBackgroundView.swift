2a4e4ad1b8f7f5ac6ba8b71975266cf83ceedc5e
agapeace <damiragadilov1@gmail.com>
2025-06-27 14:45:11 +0500

    UPDATE: 1. MainScreen Doesn't have UISearchTextField

---
+++ Whatever/SecondScreen/SectionBackgroundView.swift
@@ -0,0 +1,12 @@
+//
+//  SectionBackgroundView.swift
+//  Whatever
+//
+//  Created by Damir Agadilov  on 26.06.2025.
+//
+
+import UIKit
+
+class SectionBackgroundView: UICollectionReusableView {
+        
+}
